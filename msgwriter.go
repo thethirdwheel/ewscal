37da85b1591842f64efc978f7641e8b793206c44
Rory Carmichael <rcarmichael@quantcast.com>
2013-08-04 01:13:15 -0700

    add script to write availability request messages (will likely be removed or librarified)

---
+++ msgwriter.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+    "encoding/xml"
+//    "time"
+    "fmt"
+    "os"
+)
+
+type Timeblock struct {
+    Bias int
+    Time string
+    DayOrder int
+    Month int
+    DayOfWeek string
+}
+
+type TimeZone struct {
+    XMLName xml.Name `xml:"t:TimeZone"`
+    Xmlns string `xml:"xmlns,attr"`
+    Bias int
+    StandardTime Timeblock
+    DaylightTime Timeblock
+}
+
+type Mailboxes struct {
+    XMLName xml.Name `xml:"MailboxDataArray"`
+    Boxes []Mailbox
+}
+
+type Mailbox struct {
+    XMLName xml.Name `xml:"t:MailboxData"`
+    EmailAddress string `xml:"t:Email>t:Address"`
+    AttendeeType string `xml:"t:AttendeeType"`
+    ExcludeConflicts bool `xml:"t:ExcludeConflicts"`
+}
+
+type TimeWindow struct {
+    StartTime string `xml:"t:StartTime"`
+    EndTime string `xml:"t:EndTime:`
+}
+
+type FreeBusyView struct {
+    XMLName xml.Name `xml:"t:FreeBusyViewOptions"`
+    TimeWindow TimeWindow `xml:"t:TimeWindow"`
+    MergedInterval int `xml:"t:MergedFreeBusyIntervalInMinutes"`
+    RequestedView string `xml:"t:RequestedView"`
+}
+
+type AvailabilityRequest struct {
+    XMLName xml.Name `xml:"GetUserAvailabilityRequest"`
+    Xmlns string `xml:"xmlns,attr"`
+    XmlnsT string `xml:"xmlns:t,attr"`
+    Tz TimeZone
+    MailboxDataArray Mailboxes
+    Fbv FreeBusyView
+}
+
+func main() {
+    tz := TimeZone{Xmlns: "http://schemas.microsoft.com/exchange/services/2006/types", Bias: 480}
+    tz.StandardTime = Timeblock{Bias: 0, Time: "02:00:00", DayOrder: 5, Month: 10, DayOfWeek: "Sunday"}
+    tz.DaylightTime = Timeblock{Bias: -60, Time: "02:00:00", DayOrder: 1, DayOfWeek: "Sunday"}
+
+    townsend := Mailbox{EmailAddress: "CR-SF2-Townsend@quantcast.com", AttendeeType: "Required"}
+    marina := Mailbox{EmailAddress: "CR-SF8-Marina@quantcast.com", AttendeeType: "Required"}
+    boxBox := Mailboxes{ Boxes: []Mailbox{townsend, marina} }
+
+    tw := TimeWindow{StartTime: "2013-08-01T00:00:00", EndTime: "2013-08-01T23:59:59"}
+    requestWindow := FreeBusyView{ MergedInterval: 60, RequestedView: "DetailedMerged" }
+    requestWindow.TimeWindow = tw
+
+    request := AvailabilityRequest{Xmlns: "http://schemas.microsoft.com/exchange/services/2006/messages", XmlnsT: "http://schemas.microsoft.com/exchange/services/2006/types", Tz: tz, MailboxDataArray: boxBox, Fbv: requestWindow}
+    enc := xml.NewEncoder(os.Stdout)
+    enc.Indent("  ", "    ")
+    if err := enc.Encode(request); err != nil {
+        fmt.Printf("error: %v\n", err)
+    }
+}
