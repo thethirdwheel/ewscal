330b9e1e66bb229365f72a53ff6be1f6311be972
Rory Carmichael <rcarmichael@quantcast.com>
2013-08-05 00:11:36 -0700

    take command line arguments for rooms of interest, start and end dates, and fix myriad small bugs in xml generation

--- msgwriter.go
+++ msgwriter.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"encoding/xml"
-	//    "time"
+	"flag"
 	"fmt"
+	"io"
+	"log"
 	"os"
+	"time"
 )
 
 type Timeblock struct {
@@ -37,7 +41,7 @@ type Mailbox struct {
 
 type TimeWindow struct {
 	StartTime string `xml:"t:StartTime"`
-	EndTime   string `xml:"t:EndTime:`
+	EndTime   string `xml:"t:EndTime"`
 }
 
 type FreeBusyView struct {
@@ -66,30 +70,52 @@ type AvailabilityEnvelope struct {
 	XmlnsXsi  string   `xml:"xmlns:xsi,attr"`
 	XmlnsXsd  string   `xml:"xmlns:xsd,attr"`
 	XmlnsSoap string   `xml:"xmlns:soap,attr"`
-	XmlnsT    string   `xml:"xmlns:t"`
+	XmlnsT    string   `xml:"xmlns:t,attr"`
 	Body      AvailabilityEnvelopeBody
 }
 
+func generateMailboxes(source io.Reader) Mailboxes {
+	boxen := make([]Mailbox, 0)
+	scanner := bufio.NewScanner(source)
+	for scanner.Scan() {
+		boxen = append(boxen, Mailbox{EmailAddress: scanner.Text(), AttendeeType: "Required"})
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return Mailboxes{Boxes: boxen}
+}
+
 func main() {
+	roomlist := flag.String("roomlist", "roomlist", "file containing the list of conference room email addresses to query")
+	startdate := flag.String("startdate", time.Now().Add(time.Hour*12).Format(time.RFC3339), "start time in RFC3339 format")
+	enddate := flag.String("enddate", time.Now().Add(time.Hour*24).Format(time.RFC3339), "end time in RFC3339 format")
+	flag.Parse()
+
+	//Build set of rooms we're interested in
+	roomfile, err := os.Open(*roomlist)
+	if err != nil {
+		log.Fatal(err)
+	}
+	boxen := generateMailboxes(roomfile)
+
+	//Set timezone information for return values
 	tz := TimeZone{Xmlns: "http://schemas.microsoft.com/exchange/services/2006/types", Bias: 480}
 	tz.StandardTime = Timeblock{Bias: 0, Time: "02:00:00", DayOrder: 5, Month: 10, DayOfWeek: "Sunday"}
-	tz.DaylightTime = Timeblock{Bias: -60, Time: "02:00:00", DayOrder: 1, DayOfWeek: "Sunday"}
+	tz.DaylightTime = Timeblock{Bias: -60, Time: "02:00:00", DayOrder: 1, Month: 4, DayOfWeek: "Sunday"}
 
-	townsend := Mailbox{EmailAddress: "CR-SF2-Townsend@quantcast.com", AttendeeType: "Required"}
-	marina := Mailbox{EmailAddress: "CR-SF8-Marina@quantcast.com", AttendeeType: "Required"}
-	boxBox := Mailboxes{Boxes: []Mailbox{townsend, marina}}
-
-	tw := TimeWindow{StartTime: "2013-08-01T00:00:00", EndTime: "2013-08-01T23:59:59"}
-	requestWindow := FreeBusyView{MergedInterval: 60, RequestedView: "DetailedMerged"}
+	//Set time window of interest
+	tw := TimeWindow{StartTime: *startdate, EndTime: *enddate}
+	requestWindow := FreeBusyView{MergedInterval: 60, RequestedView: "FreeBusy"}
 	requestWindow.TimeWindow = tw
 
-	request := AvailabilityRequest{Xmlns: "http://schemas.microsoft.com/exchange/services/2006/messages", XmlnsT: "http://schemas.microsoft.com/exchange/services/2006/types", Tz: tz, MailboxDataArray: boxBox, Fbv: requestWindow}
+	request := AvailabilityRequest{Xmlns: "http://schemas.microsoft.com/exchange/services/2006/messages", XmlnsT: "http://schemas.microsoft.com/exchange/services/2006/types", Tz: tz, MailboxDataArray: boxen, Fbv: requestWindow}
 
 	body := AvailabilityEnvelopeBody{Request: request}
 	envelope := AvailabilityEnvelope{XmlnsXsi: "http://www.w3.org/2001/XMLSchema-instance", XmlnsXsd: "http://www.w3.org/2001/XMLSchema", XmlnsSoap: "http://schemas.xmlsoap.org/soap/envelope/", XmlnsT: "http://schemas.microsoft.com/exchange/services/2006/types", Body: body}
 	enc := xml.NewEncoder(os.Stdout)
 	enc.Indent("  ", "    ")
-	fmt.Printf(xml.Header)
+	fmt.Print(xml.Header)
 	if err := enc.Encode(envelope); err != nil {
 		fmt.Printf("error: %v\n", err)
 	}
