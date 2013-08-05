4bb38039c8e2ef8dbe439c9fa0ebc02fc507e8fb
Rory Carmichael <rcarmichael@quantcast.com>
2013-08-05 00:59:14 -0700

    modify api format to suit nate's desire never to parse anything

--- ewscal.go
+++ ewscal.go
@@ -6,14 +6,22 @@ import (
 	"time"
 )
 
+/*
 type TimeWindow struct {
 	Start    time.Time
 	Duration time.Duration
 }
+*/
 
 type Room struct {
 	Name      string
-	Vacancies []TimeWindow
+    Floor     string
+    Size      int
+    Email     string
+//	Vacancies []TimeWindow
+    Start     time.Time
+    Duration  time.Duration
+    Open      bool
 }
 
 type Rooms []Room
@@ -26,9 +34,9 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, bool), all bool) ht
 
 //Mocked for now, get the data from Exchange eventually
 func getRooms(all bool) (r Rooms) {
-	r = append(r, Room{"Room1", []TimeWindow{TimeWindow{time.Now(), time.Duration(10)}, TimeWindow{time.Now(), time.Duration(200)}}})
+	r = append(r, Room{"Room1", "2nd", 4, "CR-PL2-Room1@place.com", time.Now(), time.Duration(10), true})
 	if all {
-		r = append(r, Room{"Room2", []TimeWindow{TimeWindow{time.Now(), time.Duration(60)}, TimeWindow{time.Now(), time.Duration(12)}}})
+		r = append(r, Room{"Room2", "8th", 6, "CR-PL8-Room2@place.com", time.Now().Add(time.Minute*5), time.Duration(60), false})
 	}
 	return
 }
