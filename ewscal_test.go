9b903ce058bd7fab3be0560b6560f4963381bfce
Rory Carmichael <rcarmichael@quantcast.com>
2013-08-09 11:47:50 -0700

    gofmt

--- ewscal_test.go
+++ ewscal_test.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"io/ioutil"
-	"testing"
 	"bytes"
 	"encoding/xml"
+	"io/ioutil"
 	"log"
-    "time"
+	"testing"
+	"time"
 )
 
 func TestParseAvailabilityResponse(t *testing.T) {
@@ -15,25 +15,25 @@ func TestParseAvailabilityResponse(t *testing.T) {
 	if err != nil {
 		log.Fatal("couldn't read test_response.xml: ", err)
 	}
-//    log.Print(time.RFC3339)
-    startTime, err := time.Parse(time.RFC3339, "2013-08-05T08:00:00Z")
-    if err != nil {
-        log.Fatal("couldn't parse date: ", err)
-    }
-    initRoomTimes(&r, startTime)
-    log.Printf("%v", r)
+	//    log.Print(time.RFC3339)
+	startTime, err := time.Parse(time.RFC3339, "2013-08-05T08:00:00Z")
+	if err != nil {
+		log.Fatal("couldn't parse date: ", err)
+	}
+	initRoomTimes(&r, startTime)
+	log.Printf("%v", r)
 	v := FreeBusyResponseEnvelope{}
-//	log.Printf("%v", v)
+	//	log.Printf("%v", v)
 	if err := xml.Unmarshal(testData, &v); err != nil {
 		log.Fatal("error: %v", err)
 	}
-//	log.Printf("%v", v)
+	//	log.Printf("%v", v)
 	if len(r) != len(v.Body.Response.ResponseArray.Responses) {
 		t.Errorf("Length of r (%v) not equal to length of responses (%v)", len(r), len(v.Body.Response.ResponseArray.Responses))
 	}
-    updateRoomsFromResponse(&r, *bytes.NewBuffer(testData), startTime)
+	updateRoomsFromResponse(&r, *bytes.NewBuffer(testData), startTime)
 	if len(r) != len(v.Body.Response.ResponseArray.Responses) {
 		t.Errorf("Length of r (%v) not equal to length of responses (%v)", len(r), len(v.Body.Response.ResponseArray.Responses))
 	}
-    log.Printf("%v", r)
+	log.Printf("%v", r)
 }
